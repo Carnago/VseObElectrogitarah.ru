547791b1df12d2377f5606bbda5c1963179cec68
Tima <4@>
2022-05-16 23:24:05 +0300

    Dip Ver1.1

---
+++ SG.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css" type="text/css">
+	<link rel="shortcut icon" href="icon.ico" type="image/x-icon">
+	<title>SG</title>
+<style>
+	p
+	{
+		font-family: Geneva, Arial, Helvetica, sans-serif;
+        font-size: 20pt;
+        text-indent: 2em;
+        text-align: justify;
+	}
+	.mosh
+	{
+		text-align: center;
+	}
+
+</style>
+
+</head>
+<body>
+<header>
+	<nav class="dws-menu">
+		<ul>
+			<li><a href="http://vseobelectrogitarah.ru/index.php"><i class="g-main">Главная</i></a></li>
+			<li><a href="#"><i class="g-guitar"></i>Электрогитары</a>
+				<ul>
+					<li><a href="#">Формфактор</a>
+						<ul>
+							<a href="Stratocaster.php">Stratocaster</a>
+							<a href="Tele.php">Telecaster</a>
+							<a href="SG.php">SG</a>
+							<a href="FlyingV.php">Flying V</a>
+							
+
+						</ul>
+					</li>
+					<li><a href="#">Струны</a></li>
+					<li><a href="#">Как устроена</a></li>
+				</ul>	
+			</li>
+
+			<li><a href="#"><i class="g-guitarists"></i>Гитаристы</a></li>
+			<li><a href="#"><i class="g-history"></i>История</a>
+				<ul>
+					<li><a href="#">Производители гитар</a>
+						<ul>
+							<a href="#">Fender</a>
+							<a href="#">Gibson</a>
+							<a href="#">Ibanez</a>
+							<a href="epiphone.php">Epiphone</a>
+							
+
+						</ul>
+					</li>
+					<li><a href="#">Производители струн</a></li>
+					
+				</ul>
+			</li>
+			<li><a href="#"><i class="g-style"></i>Техники игры</a></li>
+		</ul>
+
+		</nav>
+	</header>
+   <table align="center" border="4" bordercolor="#000000" width="70%" height="4450px" bgcolor="#FFF5EE" cellpadding="25px">
+        <tr>
+			<td valign="top"><p>В 1958 году продажи гитары Gibson Les Paul были значительно ниже, чем в предыдущие годы. В следующем году компания Gibson изготовила тонкий, с плоскими концами корпус из красного дерева, имевший двойной вырез, который позволял верхним ладам быть более доступными.Место соединения грифа и корпуса было перенесено на три лада выше для дальнейшего облегчения доступа к верхним ладам. Производственные затраты значительно сократились из-за простой конструкции корпуса, а также благодаря тому факту, что новые Les Paul были объявлены, как имеющие «самое быстрое соединение грифа и корпуса в мире» из-за его тонкого профиля и небольшого крена.Новая Les Paul была популярной, но Лес Пола не устраивал новый дизайн, поэтому он попросил удалить своё имя с модели <p class="mosh"><img src="sg.jpg" alt="Здесь могла бы быть ваша реклама"> Новая модель по просьбе Лес Пола была названа «SG», аббревиатура которой обозначала «Цельная гитара» («Solid Guitar»).Хотя название Les Paul было официально удалено в 1961 году, на SG подпись Les Paul скрывалась до конца 1963 года. Благодаря своей популярности и старинному наследию, стиль корпуса SG часто копируют другие производители, хотя гораздо реже, чем Les Paul и Fender Stratocaster. </p></p></td>
+		</tr>
+   </table>	
+</body>
+</html>
---
+++ Stratocaster.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css" type="text/css">
+	<link rel="shortcut icon" href="icon.ico" type="image/x-icon">
+	<title>Stratocaster</title>
+<style>
+	p
+	{
+		font-family: Geneva, Arial, Helvetica, sans-serif;
+        font-size: 20pt;
+        text-indent: 2em;
+        text-align: justify;
+	}
+	.mosh
+	{
+		text-align: center;
+	}
+
+</style>
+
+</head>
+<body>
+<header>
+	<nav class="dws-menu">
+		<ul>
+			<li><a href="http://vseobelectrogitarah.ru/index.php"><i class="g-main">Главная</i></a></li>
+			<li><a href="#"><i class="g-guitar"></i>Электрогитары</a>
+				<ul>
+					<li><a href="#">Формфактор</a>
+						<ul>
+							<a href="Stratocaster.php">Stratocaster</a>
+							<a href="Tele.php">Telecaster</a>
+							<a href="SG.php">SG</a>
+							<a href="FlyingV.php">Flying V</a>
+							
+
+						</ul>
+					</li>
+					<li><a href="#">Струны</a></li>
+					<li><a href="#">Как устроена</a></li>
+				</ul>	
+			</li>
+
+			<li><a href="#"><i class="g-guitarists"></i>Гитаристы</a></li>
+			<li><a href="#"><i class="g-history"></i>История</a>
+				<ul>
+					<li><a href="#">Производители гитар</a>
+						<ul>
+							<a href="#">Fender</a>
+							<a href="#">Gibson</a>
+							<a href="#">Ibanez</a>
+							<a href="epiphone.php">Epiphone</a>
+							
+
+						</ul>
+					</li>
+					<li><a href="#">Производители струн</a></li>
+					
+				</ul>
+			</li>
+			<li><a href="#"><i class="g-style"></i>Техники игры</a></li>
+		</ul>
+
+		</nav>
+	</header>
+   <table align="center" border="4" bordercolor="#000000" width="70%" height="4450px" bgcolor="#FFF5EE" cellpadding="25px">
+        <tr>
+			<td valign="top"><p>Fender Stratocaster™, (часто упоминаемый как Strat) — модель электрогитары, разработанной Джорджем Фуллертоном, Лео Фендером и Фредди Таваресом в 1954 году, и выпускаемая долгое время вплоть до наших дней. Stratocaster использовался многими соло-гитаристами и поэтому её можно услышать на многих исторических записях. Наряду с Gibson Les Paul, Gibson SG и Fender Telecaster — это одна из самых известных и распространённых моделей электрогитар в мире. <img src="strat.png">Stratocaster очень широко копируется, в результате чего название «Stratocaster» или «Strat» часто используют для обозначения любой гитары с теми же техническими особенностями, независимо от производителя.</p></td>
+		</tr>
+   </table>	
+</body>
+</html>
